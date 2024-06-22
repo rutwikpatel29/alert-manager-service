1d286765a78929460be85aac246469ac7695f78b
rutwik patel <rutwik@begenuin.com>
2024-06-22 18:07:39 +0530

    Updated Files

--- handlers/handler.py
+++ handlers/handler.py
@@ -12,7 +12,7 @@ def handle_pod_crash_looping_alert(alert):
     send_slack_notification(alert)
     
     # Scale the affected deployment
-    scale_deployment(alert)
+    # scale_deployment(alert)
     
 def send_slack_notification(alert):
     slack_webhook_url = config.SLACK_WEBHOOK_URL
@@ -26,20 +26,20 @@ def send_slack_notification(alert):
     }
     requests.post(slack_webhook_url, json=message)
 
-def scale_deployment(alert):
-    # Extract relevant information from the alert
-    namespace = alert['labels']['namespace']
-    deployment_name = alert['labels']['pod'].rsplit('-', 2)[0]  # Assuming the pod name is formatted as 'deployment-name-randomstring'
-    # Use Kubernetes API to scale the deployment (assuming you have permissions set up)
-    api_url = f"https://kubernetes.default.svc/apis/apps/v1/namespaces/{namespace}/deployments/{deployment_name}/scale"
-    headers = {"Authorization": f"Bearer {config.KUBERNETES_API_TOKEN}"}
-    payload = {
-        "spec": {
-            "replicas": 0  # Scale down to 0 to stop the crash looping
-        }
-    }
-    response = requests.put(api_url, json=payload, headers=headers, verify=False)  # Verify=False for self-signed certs in cluster
-    if response.status_code == 200:
-        print(f"Successfully scaled down deployment {deployment_name} in namespace {namespace}")
-    else:
-        print(f"Failed to scale deployment: {response.text}")
+# def scale_deployment(alert):
+#     # Extract relevant information from the alert
+#     namespace = alert['labels']['namespace']
+#     deployment_name = alert['labels']['pod'].rsplit('-', 2)[0]  # Assuming the pod name is formatted as 'deployment-name-randomstring'
+#     # Use Kubernetes API to scale the deployment (assuming you have permissions set up)
+#     api_url = f"https://kubernetes.default.svc/apis/apps/v1/namespaces/{namespace}/deployments/{deployment_name}/scale"
+#     headers = {"Authorization": f"Bearer {config.KUBERNETES_API_TOKEN}"}
+#     payload = {
+#         "spec": {
+#             "replicas": 0  # Scale down to 0 to stop the crash looping
+#         }
+#     }
+#     response = requests.put(api_url, json=payload, headers=headers, verify=False)  # Verify=False for self-signed certs in cluster
+#     if response.status_code == 200:
+#         print(f"Successfully scaled down deployment {deployment_name} in namespace {namespace}")
+#     else:
+#         print(f"Failed to scale deployment: {response.text}")
